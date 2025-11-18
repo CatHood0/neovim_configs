9f9d2277be8a2bb7f102c5d9623ccec665d6641c
CatHood0 <santiagomata1503@gmail.com>
2025-11-17 22:13:57 -0400

    Chore: remove lsp-timeout by weird issues

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -304,24 +304,6 @@ return {
     end,
     opts = {}
   },
-  -- to manage the memory management of the lsps used
-  -- by neovim, we use this
-  {
-    "hinell/lsp-timeout.nvim",
-    dependencies = { "neovim/nvim-lspconfig" },
-    init = function()
-      vim.g.lspTimeoutConfig = {
-        stopTimeout  = 1000 * 60 * 5, -- ms, timeout before stopping all LSPs
-        startTimeout = 1000 * 10,     -- ms, timeout before restart
-        silent       = false,         -- true to suppress notifications
-        filetypes    = {
-          ignore = {                  -- filetypes to ignore; empty by default
-            -- lsp-timeout is disabled completely
-          }                           -- for these filetypes
-        }
-      }
-    end
-  },
   { 'neovim/nvim-lspconfig' },
   -- we use schemas for jsonls and yamlls
   { 'b0o/schemastore.nvim' },
